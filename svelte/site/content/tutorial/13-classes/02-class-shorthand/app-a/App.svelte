1f7786e89dacb1c790c2d071a14b8725474825f0
John Doe <iam@alexxnb.ru>
2019-03-18 18:34:02 +0300

    translated classes

---
+++ svelte/site/content/tutorial/13-classes/02-class-shorthand/app-a/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	let big = false;
+</script>
+
+<style>
+	.big {
+		font-size: 4em;
+	}
+</style>
+
+<label>
+	<input type=checkbox bind:checked={big}>
+	большой
+</label>
+
+<div class:big={big}>
+	какой-то {big ? 'большой' : 'маленький'} текст
+</div>
