fa3fdda9b9e67685c5d67fe30c87c634bb6200f5
John Doe <iam@alexxnb.ru>
2019-03-19 17:03:57 +0300

    translated reactivity

---
+++ svelte/site/content/examples/01-reactivity/00-reactive-assignments/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<button on:click={handleClick}>
+	{count === 0 ? 'Кликни по мне' : `Кликов: ${count}`}
+</button>
---
+++ svelte/site/content/examples/01-reactivity/01-reactive-declarations/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	let count = 1;
+
+	// префикс `$:` означает 'пересчитать, когда переменные изменятся'
+	$: doubled = count * 2;
+	$: quadrupled = doubled * 2;
+
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<button on:click={handleClick}>
+	Кликов: {count}
+</button>
+
+<p>{count} * 2 = {doubled}</p>
+<p>{doubled} * 2 = {quadrupled}</p>
