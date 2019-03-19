1f5e425f29ad4959bf80ea6a2743dde0ec48cdc8
John Doe <iam@alexxnb.ru>
2019-03-19 17:17:47 +0300

    translated logic

---
+++ svelte/site/content/examples/03-logic/01-else-blocks/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	let user = { loggedIn: false };
+
+	function toggle() {
+		user.loggedIn = !user.loggedIn;
+	}
+</script>
+
+{#if user.loggedIn}
+	<button on:click={toggle}>
+		Выйти
+	</button>
+{:else}
+	<button on:click={toggle}>
+		Войти
+	</button>
+{/if}
---
+++ svelte/site/content/examples/03-logic/04-keyed-each-blocks/Thing.svelte
@@ -0,0 +1,9 @@
+<script>
+	// `value` обновляется при изменении значения свойства...
+	export let value;
+
+	// ...а `valueAtStart` фиксируется при инициализации
+	const valueAtStart = value;
+</script>
+
+<p>{valueAtStart} / {value}</p>
