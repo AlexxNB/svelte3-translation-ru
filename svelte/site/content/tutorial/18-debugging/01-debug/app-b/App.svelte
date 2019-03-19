6b43d35b17f6b23914c4f5f2efc49292d4651cec
John Doe <iam@alexxnb.ru>
2019-03-19 11:21:50 +0300

    translated debugging

---
+++ svelte/site/content/tutorial/18-debugging/01-debug/app-b/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	let user = {
+		firstname: 'Ада',
+		lastname: 'Лавлейс'
+	};
+</script>
+
+<input bind:value={user.firstname}>
+<input bind:value={user.lastname}>
+
+{@debug user}
+
+<h1>Привет, {user.firstname}!</h1>
