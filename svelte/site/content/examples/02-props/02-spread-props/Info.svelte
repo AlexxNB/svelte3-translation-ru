27c3b7d70ab3b9086318e5f282443efb620c5bc0
John Doe <iam@alexxnb.ru>
2019-03-19 17:09:51 +0300

    translated properties

---
+++ svelte/site/content/examples/02-props/02-spread-props/Info.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let name;
+	export let version;
+	export let speed;
+	export let website;
+</script>
+
+<p>
+	Пакет <code>{name}</code> {speed} быстрый.
+	Загрузите версию {version} из <a href="https://www.npmjs.com/package/{name}">npm</a>
+	и узнайте о нем больше <a href={website}>тут</a>
+</p>
