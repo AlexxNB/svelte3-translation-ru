04e657ffc63eee9fc904da4358be2ec0cc145b81
John Doe <iam@alexxnb.ru>
2019-04-19 15:09:12 +0300

    apply https://github.com/sveltejs/svelte/commit/f04225fa0f8d6e86034afa9d7bab3771bcb7913a

---
+++ repositories/sveltejs/svelte/site/content/tutorial/10-transitions/07-local-transitions/app-a/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { slide } from 'svelte/transition';
+
+	let showItems = true;
+	let i = 5;
+	let items = ['первый', 'второй', 'третий', 'четвертый', 'пятый', 'шестой', 'седьмой', 'восьмой', 'девятый', 'десятый'];
+</script>
+
+<style>
+	div {
+		padding: 0.5em 0;
+		border-top: 1px solid #eee;
+	}
+</style>
+
+<label>
+	<input type="checkbox" bind:checked={showItems}>
+	показать список
+</label>
+
+<label>
+	<input type="range" bind:value={i} max=10>
+
+</label>
+
+{#if showItems}
+	{#each items.slice(0, i) as item}
+		<div transition:slide>
+			{item}
+		</div>
+	{/each}
+{/if}
