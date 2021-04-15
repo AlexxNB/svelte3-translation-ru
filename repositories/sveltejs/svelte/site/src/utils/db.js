cbf1ed79f3d598ad1ec0bb4e8884d1feb2f2f594
AlexxNB <iam@alexxnb.ru>
2021-04-15 10:12:28 +0300

    revert

--- repositories/sveltejs/svelte/site/src/utils/db.js
+++ repositories/sveltejs/svelte/site/src/utils/db.js
@@ -1,10 +1,4 @@
 import { Pool } from 'pg';
-console.log(`
-PGUSER=${process.env.PGUSER}
-PGPASSWORD=${process.env.PGPASSWORD}
-PGHOST=${process.env.PGHOST}
-PGDATABASE=${process.env.PGDATABASE}
-PGPORT=${process.env.PGPORT}`);
 
 // Uses `PG*` ENV vars
 export const DB = process.env.PGHOST ? new Pool({connectionTimeoutMillis:10000}) : null;
