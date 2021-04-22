26b98b062ab50f7c6729638c17323cac845ceb67
AlexxNB <iam@alexxnb.ru>
2021-04-22 10:22:12 +0300

    close #90

--- repositories/sveltejs/svelte/site/content/tutorial/05-events/06-dom-event-forwarding/app-b/CustomButton.svelte
+++ repositories/sveltejs/svelte/site/content/tutorial/05-events/06-dom-event-forwarding/app-b/CustomButton.svelte
@@ -1,20 +1,20 @@
 <style>
   button {
-    height: 4rem;
-    width: 12rem;
-    background-color: #aaa;
-    border-color: #f1c40f;
-    color: #f1c40f;
-    font-size: 1.25rem;
-    background-image: linear-gradient(45deg, #f1c40f 50%, transparent 50%);
-    background-position: 100%;
-    background-size: 400%;
-    transition: background 300ms ease-in-out;
-  }
-  button:hover {
-    background-position: 0;
-    color: #aaa;
-  }
+		height: 4rem;
+		width: 8rem;
+		background-color: #ddd;
+		border-color: #ff3e00;
+		color: #ff3e00;
+		font-size: 1.25rem;
+		background-image: linear-gradient(45deg, #ff3e00 50%, transparent 50%);
+		background-position: 100%;
+		background-size: 400%;
+		transition: background 300ms ease-in-out;
+	}
+	button:hover {
+		background-position: 0;
+		color: #ddd;
+	}
 </style>
 
 <button on:click>Нажми на меня</button>
