7323f919875bf2973fd66e81eb85ede20abdba21
Valexr <valexr@gmail.com>
2021-07-09 08:57:07 +0300

    close #121

---
+++ repositories/sveltejs/svelte/site/content/tutorial/10-transitions/09-key-blocks/app-b/App.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { fly } from 'svelte/transition';
+
+    let number = 0;
+</script>
+
+<div>
+    Номер:
+    {#key number}
+        <span style="display: inline-block" in:fly={{ y: -20 }}>
+            {number}
+        </span>
+    {/key}
+</div>
+<br />
+<button
+    on:click={() => {
+        number += 1;
+    }}>
+    Увеличить
+</button>
